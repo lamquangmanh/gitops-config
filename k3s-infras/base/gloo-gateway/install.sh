9060cf1b6c251cac24c440658641fc6233a89bbc
manh-lam-tpv-clv <manh.lam.tpv@one-line.com>
2025-05-11 10:14:22 +0700

    feat: update install.sh

--- k3s-infras/base/gloo-gateway/install.sh
+++ k3s-infras/base/gloo-gateway/install.sh
@@ -5,4 +5,6 @@ helm repo update
 helm install gloo gloo/gloo \
   --namespace gloo-system \
   --create-namespace \
-  -f gloo-values.yaml
+  -f gloo-values.yaml \
+  --set gatewayProxies.gatewayProxy.service.type=NodePort
+  # Or use NodePort if you're in Minikube/k3s
